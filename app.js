42ee2cd4118d2ef401742057016aaa3d3d692ea9
Jack Amsterdam <64046793+jackamsterdam@users.noreply.github.com>
2021-08-23 14:31:21 +0300

    add form validation

--- app.js
+++ app.js
@@ -4,20 +4,47 @@ let parameterC = document.querySelector('#parameter-c')
 let calculateBtn = document.querySelector('#button')
 let x1 = document.querySelector('#x1')
 let x2 = document.querySelector('#x2')
-
+let errorMsg = document.querySelector('.injection-error')
 
 
 // if (Math.sign(-1) === -1) console.log('hhhhhh')
 // Hint: To print the x² to the console, use this: string: 'x\u00B2'
 
 function calculate() {
+    errorMsg.style.display = 'none'
+
     console.log('hello - you just pressed the button')
+        // not good: pops up a alert automatically: let alertNumbers = alert('Please enter numbers only')
 
     let parameter_A_Value = +parameterA.value
+    if (parameter_A_Value === 0) {
+        // alert(`You can't insert 0 as the 'a' parameter for x\u00B2`)
+        errorMsg.style.display = '';
+        errorMsg.innerText = `You can't insert 0 as the 'a' parameter for x\u00B2`
+        return
+    }
+    if (isNaN(parameter_A_Value)) {
+        //alert('Please enter numbers only')
+        errorMsg.style.display = '';
+        errorMsg.innerText = `Please enter numbers only`
+        return;
+    }
     let parameter_B_Value = +parameterB.value
+    if (isNaN(parameter_B_Value)) {
+        //alert('Please enter numbers only')
+        errorMsg.style.display = '';
+        errorMsg.innerText = `Please enter numbers only`
+        return
+    }
     let parameter_C_Value = +parameterC.value
-        // let x1_Value = +x1.value
-        // let x2_Value = +x2.value
+    if (isNaN(parameter_C_Value)) {
+        // alert('Please enter numbers only')
+        errorMsg.style.display = '';
+        errorMsg.innerText = `Please enter numbers only`
+        return
+    }
+    // let x1_Value = +x1.value
+    // let x2_Value = +x2.value
     console.log('parameter_A_Value', parameter_A_Value)
     console.log('parameter_B_Value', parameter_B_Value)
     console.log('parameter_C_Value', parameter_C_Value)
@@ -26,13 +53,17 @@ function calculate() {
 
     console.log(`${parameter_A_Value}x\u00B2 + ${parameter_B_Value}x + ${parameter_C_Value} = 0`);
     let div = document.querySelector('.injection')
+    div.style.display = '';
     div.innerHTML = `${parameter_A_Value}x\u00B2 + ${parameter_B_Value}x + ${parameter_C_Value} = 0`
 
     let discriminant = (parameter_B_Value ** 2) - (4 * parameter_A_Value * parameter_C_Value)
     console.log('discriminant', discriminant)
 
     if (discriminant < 0) {
-        alert('Your discriminant is negative, Please enter different numbers')
+        //alert('Your discriminant is negative, Please enter different numbers')
+        errorMsg.style.display = '';
+        errorMsg.innerText = `Your discriminant is negative, Please enter different numbers`
+        return
     }
 
     let positiveAnswer = (-(parameter_B_Value) + Math.sqrt(discriminant)) / (2 * parameter_A_Value)
